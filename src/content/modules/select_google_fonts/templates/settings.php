ad542c326bb013949ee1d2b59b34a8f46c736d71
Ulrich Schmidt <admin@deruli.de>
2017-09-12 18:28:27 +0200

    css

--- src/content/modules/select_google_fonts/templates/settings.php
+++ src/content/modules/select_google_fonts/templates/settings.php
@@ -1,13 +1,12 @@
 <?php
-$controller = ControllerRegistry::get ( "DesignSettingsController" );
-$fonts = $controller->getGoogleFonts ();
-$currentGoogleFont = Settings::get ( "google-font" );
+$controller = ControllerRegistry::get("DesignSettingsController");
+$fonts = $controller->getGoogleFonts();
+$currentGoogleFont = Settings::get("google-font");
 ?>
 <?php
 
-
-if (Request::hasVar ( "save" )) {
-	?>
+if (Request::hasVar("save")) {
+    ?>
 <div class="alert alert-success alert-dismissible fade in" role="alert">
 	<button type="button" class="close" data-dismiss="alert"
 		aria-label="Close">
@@ -22,17 +21,19 @@ if (Request::hasVar ( "save" )) {
 <ol class="google-fonts">
 <?php
 
-foreach ( $fonts as $font ) {
-	?>
+foreach ($fonts as $font) {
+    ?>
 <li><input type="radio" name="google-font"
 		id="label-<?php echo md5($font);?>"
 		value="<?php Template::escape($font);?>"
 		<?php
-	
-if ($font == $currentGoogleFont) {
-		?> checked <?php
-	}
-	?>> <label for="label-<?php echo md5($font);?>" style="font-family:<?php echo Template::escape($font);?>; font-size:110%;">
+    
+    if ($font == $currentGoogleFont) {
+        ?> checked
+		<?php
+    }
+    ?>> <label for="label-<?php echo md5($font);?>" style="font-family:<?php echo Template::escape($font);?>; font-size:110%;"
+	title="<?php Template::escape($font);?>">
     <?php translate("font_preview_text");?></label></li>
 <?php
 }
