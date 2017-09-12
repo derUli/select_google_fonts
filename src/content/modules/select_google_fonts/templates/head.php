d623b24d85b0f8136b52ba897d6303cec5ba4f80
Ulrich Schmidt <u.schmidt@velian.de>
2017-09-12 16:01:37 +0200

    build.json hinzugefügt, code eingerückt

--- src/content/modules/select_google_fonts/templates/head.php
+++ src/content/modules/select_google_fonts/templates/head.php
@@ -1,10 +1,12 @@
 <?php
-$controller = ControllerRegistry::get("DesignSettingsController");
-$fonts = $controller->getGoogleFonts();
+$controller = ControllerRegistry::get ( "DesignSettingsController" );
+$fonts = $controller->getGoogleFonts ();
 ?>
 
-<link rel="stylesheet" type="text/css" href="<?php echo ModuleHelper::buildModuleRessourcePath("select_google_fonts", "css/style.css");?>"/>
+<link rel="stylesheet" type="text/css"
+	href="<?php echo ModuleHelper::buildModuleRessourcePath("select_google_fonts", "css/style.css");?>" />
 <?php foreach($fonts as $font){ ?>
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=<?php Template::escape($font);?>"/>
-  <?php
+<link rel="stylesheet" type="text/css"
+	href="//fonts.googleapis.com/css?family=<?php Template::escape($font);?>" />
+<?php
 }
